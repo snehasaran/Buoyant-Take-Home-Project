fc52508c0206d7f6da8b5961f2fd20e421e45854
Sneha Saran <saran.sn@husky.neu.edu>
2017-05-15 11:57:58 -0700

    Refining code
    Refining code

--- src/main/java/com/example/demo/ApplicationController.java
+++ src/main/java/com/example/demo/ApplicationController.java
@@ -1,22 +1,17 @@
 package com.example.demo;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import javax.xml.ws.ResponseWrapper;
-import javax.xml.ws.soap.AddressingFeature.Responses;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +20,8 @@ import org.slf4j.LoggerFactory;
 public class ApplicationController {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	RestTemplate restTemplate = new RestTemplate();
+	@Value("${spring.datasource.docker_ip}")
+	private String host;
 
 	@RequestMapping("/health")
 	public ApplicationObject getHealth() {
@@ -44,32 +41,34 @@ public class ApplicationController {
 	}
 
 	@RequestMapping(method=RequestMethod.PUT, value="/shift/{value}")
-	public void update(@PathVariable String value) {
+	public HttpStatus update(@PathVariable String value) {
 		log.info("value =" + value);
 		int redis2_data = Integer.parseInt(value);
 		int redis1_data = 100-redis2_data;
-		updateData(redis1_data,redis2_data);
+		return updateData(redis1_data,redis2_data);
 	}
 
-	private void updateData(int r1, int r2){
+	private HttpStatus updateData(int r1, int r2){
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Content-Type", "application/dtab");
-		String url = "http://$DOCKER_IP:4180/api/1/dtabs/default";
-		String requestBody = "/svc => /#/io.l5d.fs; /svc/redis=> "+r1+"*/svc/redis1 & "+r2+"*/svc/redis2;";
+		String url = "http://"+host+":4180/api/1/dtabs/default";
+		String requestBody = "/svc => /#/io.l5d.fs; /svc/redis=> "
+				+r1+"*/svc/redis1 & "+r2+"*/svc/redis2;";
 		HttpEntity<String> entity = new HttpEntity<String>(requestBody, headers);
-		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
+		ResponseEntity<String> response = 
+				restTemplate.exchange(url, HttpMethod.PUT, entity, String.class);
 		log.info(response.getBody());
 		log.info(response.getHeaders().toString());
 		log.info(response.toString());
+		return response.getStatusCode();
 
 	}
-
-
 
 	private String getHealth(String uri){
 		if(uri.contains("9992") || uri.contains("3000")){
 			try{
-				ResponseEntity<String> response = restTemplate.exchange(uri,HttpMethod.GET,null,String.class);
+				ResponseEntity<String> response = 
+						restTemplate.exchange(uri,HttpMethod.GET,null,String.class);
 				log.info(response.toString());
 				return  response.getStatusCode().is2xxSuccessful() ?"up":"down";
 			}catch(Exception e){
@@ -77,39 +76,38 @@ public class ApplicationController {
 			}
 		}else{
 			try{
-				ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET,null,String.class);
+				ResponseEntity<String> response = 
+						restTemplate.exchange(uri, HttpMethod.GET,null,String.class);
 				log.info(response.toString());
 				if (!response.getStatusCode().is2xxSuccessful()){
 					return "down";
 				}else{
-					return restTemplate.getForObject(uri, String.class).equals("pong")?"up":"down";
+					return restTemplate.getForObject(uri, String.class)
+							.equals("pong")?"up":"down";
 				}
 			}catch(Exception e){
 				return "down";
 			}
 		}
-
 	}
 
 	private String getLinkerdHealth(){
-		final String uri = "http://$DOCKER_IP:9990/admin/ping";
+		final String uri = "http://"+host+":9990/admin/ping";
 		return getHealth(uri);
-
 	}
 
 	private String getNamerdHealth(){
-		final String uri = "http://$DOCKER_IP:9991/admin/ping";
+		final String uri = "http://"+host+":9991/admin/ping";
 		return getHealth(uri);
 	}
 
 	private String getLinkerdTcpHealth(){
-		final String uri = "http://$DOCKER_IP:9992/metrics";
+		final String uri = "http://"+host+":9992/metrics";
 		return getHealth(uri);
-
 	}
 
 	private String getLinkerdVizHealth(){
-		final String uri = "http://$DOCKER_IP:3000";
+		final String uri = "http://"+host+":3000";
 		return getHealth(uri);
 	}
 }
--- src/main/java/com/example/demo/ApplicationObject.java
+++ src/main/java/com/example/demo/ApplicationObject.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
 public class ApplicationObject {
-	
-	//private final long id;
-    //private final String content;
     private final String linkerd;
     private final String namerd;
     private final String linkerd_tcp;
     private final String linkerd_viz;
     
-    public ApplicationObject(String linkerd, String namerd, String linkerd_tcp, String linkerd_viz) {
+    public ApplicationObject(String linkerd, String namerd, 
+    		String linkerd_tcp, String linkerd_viz) {
     	this.linkerd = linkerd;
     	this.namerd = namerd;
     	this.linkerd_tcp = linkerd_tcp;
-    	this.linkerd_viz = linkerd_viz;
-    	
+    	this.linkerd_viz = linkerd_viz;	
     }
 	
     public String getLinkerd() {
@@ -30,20 +27,5 @@ public class ApplicationObject {
 	
 	public String getLinkerd_viz() {
 		return linkerd_viz;
-	}
-    
-    /*public ApplicationObject(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getContent() {
-        return content;
-    }*/
-    
-   
+	}   
 }
